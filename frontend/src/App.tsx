0becce8223204f2049f5b34879e101f7ef6a1b76
Pixel48 <piksel48@gmail.com>
2024-01-17 11:48:55 +0100

    Setup vite react-ts & tailwind boilderplate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl m-auto text-center text-red-500 font-bold">
+        If this is red, then tailwind is working!
+      </h1>
+    </>
+  );
+}
+
+export default App;
