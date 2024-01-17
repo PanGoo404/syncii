d24efa0ba05cd8114e6e75aba0a3c26a1d638481
Pixel48 <piksel48@gmail.com>
2024-01-17 04:56:46 +0100

    Add logging & body parsing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { PORT, MONGO } from './config.js';
 import mongoose from 'mongoose';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 
 await mongoose.connect(MONGO).then(() => {
   const { host, port, name } = mongoose.connection;
@@ -8,6 +10,10 @@ await mongoose.connect(MONGO).then(() => {
 });
 
 const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for Insomnia
+app.use(cookieParser());
 
 app.use('/', (req, res) => {
   res.status(200).json({ message: 'ACK' });
